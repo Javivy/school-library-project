d12a5dda76d939243c6475a0f8f882b587dbb7ad
Javivy <84439052+Javivy@users.noreply.github.com>
2023-04-10 14:54:38 -0400

    Initialize class and add getter and setters

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,11 @@
 require './person'
 
 class Teacher < Person
-  
+  attr_accessor :specialization
+
+  def initialize(age, specialization, name: 'Unkown', parent_permission: true)
+    super(age, name:, parent_permission:)
+
+    @specialization = specialization
+  end
 end
