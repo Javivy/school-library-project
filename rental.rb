50296eb6c8bd75fba89a6e2f7128b3301511314c
Javivy <84439052+Javivy@users.noreply.github.com>
2023-04-12 13:17:23 -0400

    Add belongs-to relationship in rental class

--- rental.rb
+++ rental.rb
@@ -11,4 +11,14 @@ class Rental
     @book = book
     book.rentals << self
   end
+
+  def person=(person)
+    @person = person
+    person.rentals.push(self) unless person.rentals.include?(self)
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
 end
